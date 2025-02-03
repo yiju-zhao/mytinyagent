05fe57c4449fe4f7105c07e8479436821c014c2e
jiwen <35399817+jiwenyu0531@users.noreply.github.com>
2025-02-03 00:43:37 -0500

    fixed the db model issues and pass the local verification

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class Conference(Base):
     )
 
     def __repr__(self):
-        return f"<Conference(name={self.name})>"
+        return f"<Conference(id={self.conference_id}, name={self.name})>"
 
 class ConferenceInstance(Base):
     __tablename__ = "conference_instance"
@@ -37,9 +37,11 @@ class ConferenceInstance(Base):
 
     # 定义与 `Conference` 表的关系
     conference = relationship("Conference", back_populates="instances")
+    # 定义与 `Paper` 表的关系
+    papers = relationship("Paper", back_populates="instance")
 
     def __repr__(self):
-        return f"<ConferenceInstance(name={self.name}， year={self.year})>"
+        return f"<ConferenceInstance(id={self.instance_id}, name={self.name}， year={self.year})>"
 
 class Paper(Base):
     __tablename__ = "paper"
@@ -61,21 +63,22 @@ class Paper(Base):
     supplementary_material_url = Column(String(255))  # 补充材料链接
 
     # 定义与 `ConferenceInstance` 表的关系
-    instance = relationship("ConferenceInstance", backref="papers")
-    # 多对多关系配置
-    authors = relationship("Author", secondary="paper_authors", back_populates="papers")
-    # 定义与 Keyword 表的多对多关系，通过 paper_keywords 中间表
+    instance = relationship("ConferenceInstance", back_populates="papers")
+    # 定于与 ContentEmbeding的关系
+    embeddings = relationship("ContentEmbedding", back_populates="paper")
+    # 定义与 authoer的关系
+    authors = relationship("PaperAuthors", back_populates="paper")
+    # 定义与 keyword的关系
     keywords = relationship("Keyword", secondary="paper_keywords", back_populates="papers")
-    # 定义与 Reference 表的多对多关系，通过 paper_references 中间表
+    # 定义与 reference 表的多对多关系，通过 paper_references 中间表
     references = relationship("Reference", secondary="paper_references", back_populates="papers")
 
     # 创建索引，方便通过标题进行快速查找
     __table_args__ = (
         Index('idx_paper_title', 'title'),
     )
-
     def __repr__(self):
-        return f"<Paper(title={self.title}, year={self.year}, TLDR={self.TLDR})>"
+        return f"<Paper(id={self.paper_id},title={self.title}, year={self.year}, TLDR={self.TLDR})>"
 
 class ContentEmbedding(Base):
     __tablename__ = "content_embedding"
@@ -83,14 +86,14 @@ class ContentEmbedding(Base):
     embedding_id = Column(Integer, primary_key=True, autoincrement=True)  # 自增主键
     paper_id = Column(Integer, ForeignKey('paper.paper_id'), nullable=False)  # 外键，关联 `paper` 表
     embedding = Column(ARRAY(Float), nullable=False)  # 存储 768 维的论文内容向量
-
     # 定义与 `Paper` 表的关系
-    paper = relationship("Paper", backref="embeddings")
-
-    # 创建 HNSW 索引
+    paper = relationship("Paper", back_populates="embeddings")
+    # 创建 HNSW 索引 -  sqlachemy 不支持hnsw索引，可以考虑通过数据库来创建
     # __table_args__ = (
     #    Index('idx_content_embedding_hnsw', 'embedding', postgresql_using='gin'),
     #)
+    def __repr__(self):
+        return f"<Reference(id={self.embedding_id}, paper_id={self.paper_id})>"
 
 class Author(Base):
     __tablename__ = "author"
@@ -102,22 +105,20 @@ class Author(Base):
     home_website = Column(String(255))  # 个人主页
     nationality = Column(String(100))  # 国籍
     # 多对多关系配置
-    papers = relationship("Paper", secondary="paper_authors", back_populates="authors")
-    # 与 Affiliation 的多对多关系
-    affiliations = relationship(
-        "Affiliation",  # 目标表是 Affiliation
-        secondary="author_affiliation",  # 通过 author_affiliation 连接表
-        back_populates="authors"  # 在 Affiliation 中定义反向关系
-    )
+    papers = relationship("PaperAuthors", back_populates="author")
+    affiliations = relationship("Affiliation",secondary="author_affiliation",back_populates="authors")
+
+    def __repr__(self):
+        return f"<Reference(id={self.author_id}, name={self.name}, email={self.email})>"
+
 
 class PaperAuthors(Base):
     __tablename__ = "paper_authors"
     
     paper_id = Column(Integer, ForeignKey('paper.paper_id'), primary_key=True)  # 外键，关联 `paper` 表
     author_id = Column(Integer, ForeignKey('author.author_id'), primary_key=True)  # 外键，关联 `author` 表
-    author_order = Column(Integer, nullable=False)  # 作者排序
+    author_order = Column(Integer, default=0)  # 作者排序
     is_corresponding = Column(Boolean, default=False)  # 是否为通讯作者
-
     # 定义与 `Paper` 和 `Author` 表的关系
     paper = relationship("Paper", back_populates="authors")
     author = relationship("Author", back_populates="papers")
@@ -131,21 +132,18 @@ class Affiliation(Base):
     location = Column(String(255))              # 地点
     website = Column(String(255))               # 网站
     description = Column(Text)                  # 描述
-
     authors = relationship(
         "Author",  # 目标表是 Author
         secondary="author_affiliation",  # 通过 author_affiliation 连接表
         back_populates="affiliations"  # 在 Author 中定义反向关系
     )
-
+    def __repr__(self):
+        return f"<Paper(id={self.affiliation_id},name={self.name}, type={self.type})>"
+    
 class AuthorAffiliation(Base):
     __tablename__ = "author_affiliation"
-    
     author_id = Column(Integer, ForeignKey('author.author_id', ondelete='CASCADE'), primary_key=True)  # 关联作者
     affiliation_id = Column(Integer, ForeignKey('affiliation.affiliation_id', ondelete='CASCADE'), primary_key=True)  # 关联组织
-    
-    author = relationship("Affiliation", back_populates="authors")
-    affiliation = relationship("Author", back_populates="affiliations")
 
 # Keyword 表模型
 class Keyword(Base):
@@ -154,22 +152,15 @@ class Keyword(Base):
     keyword_id = Column(Integer, primary_key=True)  # 自增主键
     keyword = Column(String(255), unique=True, nullable=False)  # 关键字，不能为空，唯一
     description = Column(Text)  # 关键字的描述
-
     # 定义与 Paper 表的多对多关系，通过 paper_keywords 中间表
     papers = relationship("Paper", secondary="paper_keywords", back_populates="keywords")
-
     def __repr__(self):
-        return f"<Keyword(keyword={self.keyword}, description={self.description})>"
+        return f"<Keyword(id={self.keyword_id}, keyword={self.keyword}, description={self.description})>"
 
 class PaperKeyword(Base):
     __tablename__ = 'paper_keywords'
-
-    paper_id = Column(Integer, primary_key=True)
-    keyword_id = Column(Integer, primary_key=True)
-
-    # 外键约束
-    paper = relationship("Paper", back_populates="keywords")
-    keyword = relationship("Keyword", back_populates="papers")
+    paper_id = Column(Integer, ForeignKey('paper.paper_id', ondelete='CASCADE'), primary_key=True)  # 关联作者
+    keyword_id = Column(Integer, ForeignKey('keyword.keyword_id', ondelete='CASCADE'), primary_key=True)  # 关联组织
 
 class Reference(Base):
     __tablename__ = 'reference'
@@ -180,19 +171,15 @@ class Reference(Base):
     year = Column(Integer)  # 参考文献出版年份
     journal = Column(String(255))  # 参考文献所属期刊名称
     web_url = Column(String(255))  # 参考文献的网页 URL 或指向原始论文的 URL
-
     # 定义与 Paper 表的多对多关系，通过 paper_references 中间表
     papers = relationship("Paper", secondary="paper_references", back_populates="references")
-
     def __repr__(self):
-        return f"<Reference(title={self.title}, author={self.author}, year={self.year})>"
+        return f"<Reference(id={self.reference_id}, title={self.title}, author={self.author}, year={self.year})>"
 
 class PaperReference(Base):
-    __tablename__ = 'paper_references'
-    
-    paper_id = Column(Integer, primary_key=True)
-    reference_id = Column(Integer, primary_key=True)
+    __tablename__ = 'paper_references' 
+    paper_id = Column(Integer, ForeignKey('paper.paper_id', ondelete='CASCADE'), primary_key=True)  # 关联作者
+    reference_id = Column(Integer, ForeignKey('reference.reference_id', ondelete='CASCADE'), primary_key=True)  # 关联组织
 
-    # 外键约束
-    paper = relationship("Paper", back_populates="references")
-    reference = relationship("Reference", back_populates="papers")
+
+
